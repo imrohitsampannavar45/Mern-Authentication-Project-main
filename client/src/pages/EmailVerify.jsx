abf5576bb5bfed90cbc6f657c73f9e82b7f67c41
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-12-11 22:40:37 +0530

    new update

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { assets } from '../assets/assets'
+import axios from 'axios'
+import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 const EmailVerify = () => {
 
+    axios.defaults.withCredentials = true;
+    const { backendUrl, isLoggedIn, userData, getUserData } = useContext(AppContext)
 
+    const navigate = useNavigate();
 
     const inputRefs = React.useRef([])
 
@@ -30,20 +37,35 @@ const EmailVerify = () => {
         })
     }
 
-    const onSubmitHandler = async((e) => {
+    const onSubmitHandler = async (e) => {
         try {
+            e.preventDefault();
+            const otpArray = inputRefs.current.map(e => e.value)
+            const otp = otpArray.join('')
 
+            const { data } = await axios.post(backendUrl + '/api/auth/verify-account', { otp })
+
+            if (data.success) {
+                toast.success(data.message)
+                getUserData()
+                navigate('/')
+            } else {
+                toast.error(data.message)
+            }
         } catch (error) {
-
+            toast.error(error.message);
         }
-    })
+    }
+    useEffect(() => {
+
+    }, [isLoggedIn, userData])
 
 
     return (
         <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
             <img onClick={() => navigate('/')} className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' src={assets.logo} alt="" />
 
-            <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+            <form onSubmit={onSubmitHandler} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
 
                 <h1 className='text-white text-2xl font-semibold text-center mb-4'>Email Verify OTP </h1>
                 <p className='text-center mb-6 text-indigo-300'>Enter the 6-digit code sent to your email id </p>
