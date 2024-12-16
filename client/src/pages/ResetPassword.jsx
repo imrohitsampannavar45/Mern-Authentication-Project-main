7f53cc79797ebe8ef89125ebb17f2a73abe0ad0d
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-12-16 21:38:41 +0530

    changes

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { assets } from '../assets/assets'
 const ResetPassword = () => {
     return (
-        <div>
-            Reset Password
+        <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
+            <img onClick={() => navigate('/')} className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' src={assets.logo} alt="" />
         </div>
     )
 }
